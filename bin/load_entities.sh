5ab856f2f2f82fbbdda807c8a93fe515417a5b09
keeagnsmith21 <keegan.r.s21@gmail.com>
2025-09-08 16:08:41 +0800

    e2e tests use load script

--- bin/load_entities.sh
+++ bin/load_entities.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# This loads all of the entities into the D1 database.
+# This script shouldn't be necessary, but wrangler is extremely unstable and complains when processing
+# too much data at once.
+# So instead, we split the sql file into batches of 250 statements and load them one at a time.
+# If wrangler ever gets fixed, we can remote this script.
+# - Keegan Smith 09/25
+
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <root_directory>"
     exit 1
@@ -34,13 +41,13 @@ for ((i=0; i<TOTAL; i+=BATCH_SIZE)); do
             npx wrangler d1 execute DB --command="$stmt" --local --config "${WORKER_DIR}/wrangler.local.toml"
             EXIT_CODE=$?
             if [ $EXIT_CODE -ne 0 ]; then
-                echo "❌ Statement failed:"
+                echo "Statement failed to load:"
                 echo "$stmt"
                 exit $EXIT_CODE
             fi
         done
     fi
 done
-echo "✅ All statements executed successfully."
+echo "All statements executed successfully."
 
 
